f866429a409531fca75f6c63e5e0ef8cf0cdabc7
MinnaChae <142373972+MinnaChae@users.noreply.github.com>
2023-11-25 18:47:04 -0800

    Add files via upload

---
+++ DrawingProgram/test_DrawingProgram.py
@@ -0,0 +1,10 @@
+from DrawingProgram import DrawingProgram
+from shapes import Shape, Circle
+
+
+def dp_test():
+    dp = DrawingProgram()
+    dp.add_shape(Circle("Circle", 2))
+    # print(dp)x
+
+dp_test()
