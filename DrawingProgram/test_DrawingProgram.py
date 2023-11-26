ffdbcbbc682856212269bb89cfc1705ca98d5d53
MinnaChae <142373972+MinnaChae@users.noreply.github.com>
2023-11-25 21:57:37 -0800

    Add files via upload
    V2 Uploaded 11.25 @ 10pm

--- DrawingProgram/test_DrawingProgram.py
+++ DrawingProgram/test_DrawingProgram.py
@@ -1,10 +1,15 @@
 from DrawingProgram import DrawingProgram
-from shapes import Shape, Circle
+from shapes import Shape, Circle, Rectangle, Triangle, Square
 
 
 def dp_test():
     dp = DrawingProgram()
     dp.add_shape(Circle("Circle", 2))
-    # print(dp)x
-
+    dp.add_shape(Circle("Circle", 5))
+    dp.add_shape(Rectangle("Rectangle", 3,4))
+    # print(dp.get_name(0))
+    print(dp)
+    # dp.set_shape(1,Triangle("Triangle", 1,2,3))
+    # print(dp)
+    # print(dp.get_shape(1))
 dp_test()
