3ff6bfa5efef7b25fb9c17cfde83e031a65f7bc8
MinnaChae <142373972+MinnaChae@users.noreply.github.com>
2023-11-26 14:33:45 -0800

    Add files via upload
    11.26 @ 2:33pm

--- DrawingProgram/DrawingProgram.py
+++ DrawingProgram/DrawingProgram.py
@@ -4,6 +4,7 @@ Drawing Program
 TCSS 502
 """
 from shapes import Shape, Circle, Rectangle, Triangle, Square
+from ShapeFactory import ShapeFactory
 
 
 class DrawingProgramMain:
@@ -29,44 +30,47 @@ class DrawingProgram():
         # __str__: returns a string representation of each of the shapes --
         # each shape will be separated from others by a newline (\n)
         # creating each shape's array to "sort" them
-        list_rec = []
-        list_sq = []
-        list_cir = []
-        list_tri = []
-        # print(self.name)
+        # list_rec = []
+        # list_sq = []
+        # list_cir = []
+        # list_tri = []
+        # # print(self.name)
+        #
+        # #looping through list and placing Shape objects into their specific array
+        # for shape in self.list_shapes:
+        #     if shape.get_name() == "Circle":
+        #         list_cir.append(shape)
+        #     if shape.get_name() == "Rectangle":
+        #         list_rec.append(shape)
+        #     if shape.get_name() == "Square":
+        #         list_sq.append(shape)
+        #     if shape.get_name() == "Triangle":
+        #         list_tri.append(shape)
+        #
+        # #looping through the arrays to put into a string. Need more conditions
+        # #to check for empty arrays. This code will probably not be the final code
+        # for shape in list_cir:
+        #     self.string += str(shape) + ", "
+        # self.string += "\n"
+        # for shape in list_rec:
+        #     self.string += str(shape) + ", "
+        # for shape in list_sq:
+        #     self.string += str(shape) + ", "
+        # self.string += "\n"
+        # for shape in list_tri:
+        #     self.string += str(shape) + ", "
+        # self.string += "\n"
 
-        #looping through list and placing Shape objects into their specific array
-        for shape in self.list_shapes:
-            if shape.get_name() == "Circle":
-                list_cir.append(shape)
-            if shape.get_name() == "Rectangle":
-                list_rec.append(shape)
-            if shape.get_name() == "Square":
-                list_sq.append(shape)
-            if shape.get_name() == "Triangle":
-                list_tri.append(shape)
+        formatted_list = [str(shape) for shape in self.list_shapes]
+        return "\n".join(formatted_list) + "\n"
 
-        #looping through the arrays to put into a string. Need more conditions
-        #to check for empty arrays. This code will probably not be the final code
-        for shape in list_cir:
-            self.string += str(shape) + ", "
-        self.string += "\n"
-        for shape in list_rec:
-            self.string += str(shape) + ", "
-        for shape in list_sq:
-            self.string += str(shape) + ", "
-        self.string += "\n"
-        for shape in list_tri:
-            self.string += str(shape) + ", "
-        self.string += "\n"
-
-        return f"{self.string}"
+        # return f"{self.string}"
 
 
-    def add_shape(self, Shape):  #Correct output
+    def add_shape(self, ShapeFactory):  #Correct output
         # Thoughts: is it better to sort in add_shape?
         # add_shape(Shape): a method that adds a Shape
-        self.list_shapes.append(Shape)
+        self.list_shapes.append(ShapeFactory)
         self.size +=1 #necessary??
         # print(f"in add_shape this is shape {Shape.get_name()}")
         # print(f"this is list in add_shape {self.list_shapes[0]}")
@@ -81,15 +85,70 @@ class DrawingProgram():
 
     def print_shape(self, shape):
         # print_shape(Shape): prints all shapes that match the type of the shape passed in
+        string = ""
+        for shape in self.list_shapes:
+            if shape.get_name() == "Circle":
+                string += shape
+            if shape.get_name() == "Rectangle":
+                string += shape
+            if shape.get_name() == "Square":
+                string += shape
+            if shape.get_name() == "Triangle":
+                string += shape
+        return string
+
+
         pass
 
-    def sort_shapes(self): # probably hardest module to write
+    def sort_shapes(self, *args): # probably hardest module to write
         # sort_shapes(): sorts the list/collection of shapes -- you must use a sort that runs in O(nlogn)
         # for its worst case
         # shapes will be sorted first by name, then by area if names are same
-        pass
+        # list =
 
-    def get_shape(self, index): #Correct output
+    # def
+        if len(self.list_shapes) > 1:
+            mid = len(self.list_shapes) // 2
+
+        left = self.list_shapes[:mid]
+        right = self.list_shapes[mid:]
+
+        # Recursive call on each half
+        self.sort_shapes(left)
+        self.sort_shapes(right)
+
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
+
+        # Iterator for the main list
+        k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                # The value from the left half has been used
+                self.list_shapes[k] = left[i]
+                # Move the iterator forward
+                i += 1
+            else:
+                self.list_shapes[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            self.list_shapes[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            self.list_shapes[k] = right[j]
+            j += 1
+            k += 1
+
+
+def get_shape(self, index): #Correct output
         # get_shape(index): returns the shape at the specified index
         if index >= len(self.list_shapes) or index < 0 or len(self.list_shapes) == 0:
             raise ValueError("Index cannot be greater than the length of the list")
