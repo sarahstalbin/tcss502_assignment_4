60282844f365b69a6b7c4bd0cd1fd5c8dda4feee
Sarah St. Albin <130087873+sstalbin@users.noreply.github.com>
2023-11-27 11:22:39 -0800

    Add documentation and change **kwargs parameter to *args.

--- ShapeFactory.py
+++ ShapeFactory.py
@@ -3,35 +3,27 @@ from shapes_with_docs import Circle, Square, Rectangle, Triangle
 
 class ShapeFactory:
     """
-    Creates and returns shape objects for DrawingProgram.
+    Creates and returns Shape objects for DrawingProgram. All shapes should be created via the ShapeFactory.
+
+    Contains one method: create_shape(), which creates a Shape object of a specified type along with the relevant
+    data for creating that Shape.
     """
 
     @staticmethod
-    def create_shape(shape_name, **kwargs):
+    def create_shape(shape_name, *args):
         """
-        Creates a shape of a specified type using the relevant data for building that shape. Returns the shape.
+        Creates a Shape object of a specified type using the relevant data for building that Shape. Returns the Shape.
         :param shape_name: name of the shape object to be created.
         :param kwargs: variable shape data required based on the type of shape to be created.
         :return: a Circle, Square, Rectangle, or Triangle object.
         """
         if shape_name == "circle":
-            return Circle(shape_name, **kwargs)
+            return Circle(shape_name, *args)
         elif shape_name == "square":
-            return Square(shape_name, **kwargs)
+            return Square(shape_name, *args)
         elif shape_name == "rectangle":
-            return Rectangle(shape_name, **kwargs)
+            return Rectangle(shape_name, *args)
         elif shape_name == "triangle":
-            return Triangle(shape_name, **kwargs)
+            return Triangle(shape_name, *args)
         raise AssertionError("Shape type is not valid.")
-
-
-r = ShapeFactory.create_shape("rectangle", length=3, width=5)
-s = ShapeFactory.create_shape("square", side=6)
-t = ShapeFactory.create_shape("triangle", side1=2, side2=3, side3=5)
-c = ShapeFactory.create_shape("circle", radius=5)
-
-print(r.length)
-print(s.side)
-print(t.side2)
-print(c.radius)
 
